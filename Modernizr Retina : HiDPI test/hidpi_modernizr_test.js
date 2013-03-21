a6e4b471a584125bbce2b234a1774730f6592982
Iacami <github@algumacoisa.org>
2013-03-21 08:18:22 -0300

    Update hidpi_modernizr_test.js

--- Modernizr Retina : HiDPI test/hidpi_modernizr_test.js
+++ Modernizr Retina : HiDPI test/hidpi_modernizr_test.js
@@ -1,16 +1,9 @@
 //Detect high res displays (Retina, HiDPI, etc...)
 Modernizr.addTest('highresdisplay', function(){ 
-
 	if (window.matchMedia) { 
 		var mq = window.matchMedia("only screen and (-moz-min-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
-	
 		if(mq && mq.matches) {
 			return true;
-		} else {
-			return false;
-		}  
-	
-	} else {
-		return false;
-	}
+		} 
+    }
 });
